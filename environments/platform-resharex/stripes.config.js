6457c50ec302ce4941528a09e34f4fdb2e15d77f
Ian Hardy <ian@indexdata.com>
2026-07-01 13:33:25 -0400

    Add ci (#1)
    * WIP try building full platform
    
    * adjust resharex build
    
    * add deployment workflow

---
+++ environments/platform-resharex/stripes.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  okapi: { 'url':'https://resharex-okapi.folio-dev-us-east-1-1.folio-dev.indexdata.com', 'tenant':'rs1' },
+  config: {
+    showHomeLink: true,
+    welcomeMessage: 'ReShare X Platform',
+    platformName: 'ReShare',
+    platformDescription: 'ReShare platform',
+    hasAllPerms: false,
+    showDevInfo: true,
+    staleBundleWarning: { path: '/index.html', header: 'last-modified', interval: 5 },
+  },
+  modules: {
+    "@folio/calendar": {},
+    "@folio/checkin": {},
+    "@folio/checkout": {},
+    "@folio/circulation": {},
+    "@folio/developer": {},
+    "@folio/handler-stripes-registry": {},
+    "@folio/inventory": {},
+    "@folio/myprofile": {},
+    "@folio/notes": {},
+    "@folio/plugin-create-inventory-records": {},
+    "@folio/plugin-find-instance": {},
+    "@folio/plugin-find-interface": {},
+    "@folio/plugin-find-user": {},
+    "@folio/requests": {},
+    "@folio/servicepoints": {},
+    "@folio/stripes-inventory-components": {},
+    "@folio/tags": {},
+    "@folio/tenant-settings": {},
+    "@folio/users": {},
+    "@projectreshare/rs": {},
+    "@projectreshare/rsdir": {},
+    "@projectreshare/request": {},
+    "@projectreshare/supply": {}
+  },
+  branding: {
+    style: {},
+    logo: {
+      src: './tenant-assets/reshare-logo.png',
+      alt: 'ReShare 2',
+    },
+    favicon: {
+      src: './tenant-assets/reshare-favicon.jpg',
+    },
+  },
+};
